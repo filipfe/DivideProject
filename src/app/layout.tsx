c7c0de9a1815dd58561d1571872dcfcdcf73afcc
Filip <filfer05@gmail.com>
2023-08-24 01:54:23 +0200

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
               },
             }}
           />
-          <ScrollProvider>{children}</ScrollProvider>
+          {children}
         </GAProvider>
         <Analytics />
       </body>
